191fb4b8109dea1e3d9d000e5010bcf5b09aea82
Daniel Montero Motilla <damonmo@gmail.com>
2022-02-01 10:57:40 +0100

    mame integration

--- Core/Src/porting/mame/main_mame.c
+++ Core/Src/porting/mame/main_mame.c
@@ -7,10 +7,29 @@
 #include "gw_lcd.h"
 #include "gw_linker.h"
 
+#include "mame.h"
+
+void InitCheat(void)
+{
+}
+
+void StopCheat(void)
+{
+}
+
+int cheat_menu(struct osd_bitmap *bitmap, int selection)
+{
+}
+
+void DoCheat(void)
+{
+}
 
 void app_main_mame(uint8_t load_state)
 {
+    int ret;
     printf("MAME Load: %s\n", ACTIVE_FILE->name);
+    ret = run_game(0);
 
     while (1) {
         HAL_Delay(1000);
